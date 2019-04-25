9548682c0ef975f41aef4dbfba4ac3ae21e8944b
Konrad <konradsusel@gmail.com>
2019-04-25 23:04:57 +0200

    Add getById(id) method

--- src/main/java/servlets/GameServlet.java
+++ src/main/java/servlets/GameServlet.java
@@ -11,6 +11,8 @@ import java.util.List;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import dao.GameHibernate;
 import model.Game;
+import org.hibernate.Session;
+import javax.persistence.*;
 
 @WebServlet("/game")
 public class GameServlet extends HttpServlet {
@@ -24,10 +26,28 @@ public class GameServlet extends HttpServlet {
             response.getWriter().write(mapper.writeValueAsString(gameList));
         }
 
+        if (request.getRequestURI().length() < 5) {
+            try {
+                long gameId = Long.parseLong(request.getRequestURI().split("/")[1]);
+                Game game = gameHibernate.getGameById(gameId);
+                ObjectMapper mapper = new ObjectMapper();
+                response.getWriter().write(mapper.writeValueAsString(game));
+            } catch (NoResultException e) {
+                response.getWriter().write("No game with this id");
+            }
+        }
+
     }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
+    }
+
+    protected void doPut(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        //if (request.getRequestURI().equals("/json-generator")) {
+            response.getWriter().write("tu kiedys bedzie put");
+
+        //}
     }
 
 }
